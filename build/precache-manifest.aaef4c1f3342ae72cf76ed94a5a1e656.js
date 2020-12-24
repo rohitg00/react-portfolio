bea38b801bb733ef7e9ffc47609de534dc52beb0
rohitg00 <ghumare64@gmail.com>
2020-12-24 18:42:44 +0530

    demo2

---
+++ build/precache-manifest.aaef4c1f3342ae72cf76ed94a5a1e656.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ed75c8f660903c0bfca630c9ec740fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b62cf8df8603267b1ec",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b62cf8df8603267b1ec",
+    "url": "/static/js/main.e1208808.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
